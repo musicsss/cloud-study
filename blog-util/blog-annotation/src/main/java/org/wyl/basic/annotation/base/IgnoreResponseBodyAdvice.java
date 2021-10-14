88ce48200116845f3bfc569dbd9ae38b8e279bf9
musicsss <mr1009948721@gmail.com>
2021-10-14 23:14:18 +0800

    1，提交部分注解相关笔记 2，提交部分接口相关笔记 3，完成注解模块部分代码的阅读

---
+++ blog-util/blog-annotation/src/main/java/org/wyl/basic/annotation/base/IgnoreResponseBodyAdvice.java
@@ -0,0 +1,15 @@
+package org.wyl.basic.annotation.base;
+
+/**
+ * cloud-study IgnoreResponseBodyAdvice
+ *
+ * <p>
+ * 忽略全局响应包装
+ * </p>
+ *
+ * @author ylWang
+ * @version 1.0.0
+ * @date 2021/10/14 21:11
+ */
+public @interface IgnoreResponseBodyAdvice {
+}
---
+++ blog-util/blog-annotation/src/main/java/org/wyl/basic/base/validation/IValidatable.java
@@ -0,0 +1,22 @@
+package org.wyl.basic.base.validation;
+
+/**
+ * cloud-study IValidatable
+ *
+ * <p>
+ * 实现了此接口，表示此类将会支持验证框架
+ * </p>
+ *
+ * @author ylWang
+ * @version 1.0.0
+ * @date 2021/10/14 21:06
+ */
+public interface IValidatable {
+    /**
+     * 此类需要校验什么值
+     * 支持length长度检验。也可以视情况实现支持类似于email，正则等等校验
+     *
+     * @return 需要验证的值
+     */
+    Object value();
+}
